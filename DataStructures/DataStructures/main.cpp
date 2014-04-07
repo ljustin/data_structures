740cedab3b03e737bc807eaed2e5a536fd7e07bd
ljustin <ljustin8925@yahoo.com>
2014-04-07 19:26:18 -0400

    the plan...

--- DataStructures/DataStructures/main.cpp
+++ DataStructures/DataStructures/main.cpp
@@ -2,6 +2,24 @@
 
 using namespace std;
 
+// To Do..
+
+// array
+// vector (dynamic array)
+// singly linked list
+// doubly linked list
+// Stack/Queue
+// Priority Queue / Heap
+// Hash map
+// Binary Tree
+// Binary Search Tree
+// Balanced Binary Search Tree (Red-Black Tree)
+// B-Tree
+// Graph (directed/undirected/weighted as matrix and list)
+
+// Above as templates
+
+
 int main(int argc, char** argv)
 {
     cout << "Hello World!" << endl;
